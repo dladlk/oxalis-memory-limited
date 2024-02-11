cfb3fb2f6013707e7a86678818de273f9a03ce75
Dmytro Lapko <dlk@mercell.com>
2024-02-11 13:24:10 +0200

    chore: log not only memory but also java version

--- src/test/java/network/oxalis/rd/memory/TestUtil.java
+++ src/test/java/network/oxalis/rd/memory/TestUtil.java
@@ -1,26 +1,12 @@
 package network.oxalis.rd.memory;
 
-import java.io.File;
-
 import org.slf4j.Logger;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class TestUtil {
 
 	public static void logMaxMemory(Logger logger) {
-		logger.info("**** RUNNING TEST WITH {} MB max memory ******", Runtime.getRuntime().maxMemory() / 1024.0 / 1024.0);
+		String javaVersion = System.getProperty("java.version");
+		logger.info("**** RUNNING TEST WITH {} MB max memory on java {} ******", Runtime.getRuntime().maxMemory() / 1024.0 / 1024.0, javaVersion);
 	}
-	
-	public static File getBigTestFile() {
-		String bigFilePath = "../oxalis-as4/src/test/resources/sbd-test-file-10mb.xml";
-		File bigFile = new File(bigFilePath);
-		if (!bigFile.exists()) {
-			log.warn("Cannot find for testZipBombDetectedFix a file " + bigFilePath + ", skip test");
-			return null;
-		}
-		return bigFile;
-	}
-	
+
 }
